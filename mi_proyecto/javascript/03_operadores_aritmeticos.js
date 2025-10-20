8ffa0e964c9e7ded1a20955165bd131d2edc24af
Adavidfr <adavidfruiz@gmail.com>
2025-10-20 17:15:15 -0500

    feat: se sube proyecto actualizado a la semana 3

---
+++ mi_proyecto/javascript/03_operadores_aritmeticos.js
@@ -0,0 +1,43 @@
+console.log("OPERADORES ARITMETICOS - Celulares");
+
+precio1 = 799;
+precio2 = 599;
+
+console.log("Suma");
+total = precio1 + precio2;
+console.log("Total suma:", total);
+
+console.log("Resta");
+diferencia = precio1 - precio2;
+console.log("Total resta:", diferencia);
+
+console.log("Multiplicacion");
+multiplicacion = precio1 * precio2;
+console.log("Total multiplicacion:", multiplicacion);
+
+console.log("Division");
+division = precio1 / precio2;
+console.log("Total division:", division);
+
+console.log("Modulo");
+modulo = precio1 % precio2;
+console.log("Modulo:", modulo);
+
+console.log("Potencia");
+potencia = precio1 ** 2;
+console.log("Potencia:", potencia);
+
+console.log("Operadores Comparación");
+console.log("Igualdad debil ==", "799" == 799);
+console.log("Igualdad estricta ===", "799" === 799);
+console.log("Desigualdad debil !=", "799" != 799);
+console.log("Desigualdad estricta !==", "799" !== 799);
+console.log("Mayor que >", 799 > 500);
+console.log("Menor que <", 799 < 500);
+console.log("Mayor o igual que >=", 799 >= 799);
+console.log("Menor o igual que <=", 799 <= 500);
+
+console.log("Operadores Lógicos");
+console.log("y AND &&", 799 >= 500 && 599 <= 600);
+console.log("o OR ||", 799 === 799 || 599 >= 600);
+console.log("Negación NOT !", !(799 === 799));
