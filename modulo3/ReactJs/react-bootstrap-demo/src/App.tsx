584d463156b08f905dc39138f95a1caa9ecceeba
Adavidfr <adavidfruiz@gmail.com>
2026-01-08 10:27:16 -0500

    clase 08-01-2026

--- modulo3/ReactJs/react-bootstrap-demo/src/App.tsx
+++ modulo3/ReactJs/react-bootstrap-demo/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import BsHero from "../components/bs/BsHero";
-import BsFeatures from "../components/bs/BsFeatures";
-import BsNewsletter from "../components/bs/BsNewsletter";
+import BsHero from "./components/bs/BsHero";
+import BsFeatures from "./components/bs/BsFeatures";
+import BsNewsletter from "./components/bs/BsNewsletter";
 
 export default function HomeBootstrap() {
   return (
